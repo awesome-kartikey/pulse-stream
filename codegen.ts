89ca43eae3e4e6d8fa3c23e7f37ee04d082dc57b
Kartikey Kumar <kartikeykumar.onglobe@gmail.com>
2025-04-15 02:00:05 +0530

    Update GraphQL code generation configuration to use environment variable for schema URL

--- codegen.ts
+++ codegen.ts
@@ -2,8 +2,8 @@ import type { CodegenConfig } from "@graphql-codegen/cli";
 
 const config: CodegenConfig = {
   overwrite: true,
-  schema: "http://localhost:8000/graphql",
-  documents: "**/*.{tsx,ts}",
+  schema: process.env.NEXT_PUBLIC_API_URL || "",
+  documents: ["**/*.{tsx,ts}"],
   generates: {
     "gql/": {
       preset: "client",
